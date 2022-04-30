e7ed3586ff77b3d56b860b47bec89902c1716477
Dionei J. Delangolo <dionei.jd@gmail.com>
2022-04-30 19:18:10 -0300

    Test variable types

--- build/index.js
+++ build/index.js
@@ -1,2 +1,13 @@
 "use strict";
 console.log('Started Typescript compile');
+//Check variable type
+const var1 = 'Dionei';
+const var2 = 100;
+const var3 = true;
+const var4 = { nome: 'dionei' };
+const var5 = [1, 2, 3, 4, 5];
+console.log('var1', typeof var1);
+console.log('var2', typeof var2);
+console.log('var3', typeof var3);
+console.log('var4', typeof var4);
+console.log('var5', typeof var5);
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
 console.log('Started Typescript compile')
+
+
+//Check variable type
+
+console.log('1. VARIABLE TYPES')
+const var1 = 'Dionei'
+const var2 = 100
+const var3 = true
+const var4 = { nome: 'dionei' }
+const var5 = [1, 2, 3, 4, 5]
+
+console.log('var1 => ', typeof var1)
+console.log('var2 => ', typeof var2)
+console.log('var3 => ', typeof var3)
+console.log('var4 => ', typeof var4)
+console.log('var5 => ', typeof var5)
